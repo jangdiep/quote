e1fa75105bd09916a1c939fbc08ea65d25c0b5da
jangdiep <jangdiep@gmail.com>
2020-07-22 19:15:21 -0700

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.2aaddf0eb7cc50f4747f7617739bb50c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195fe42462c9618090948b932ee2db5b",
+    "url": "/quote/index.html"
+  },
+  {
+    "revision": "92e63daf1150b402132b",
+    "url": "/quote/static/css/main.b72fdb9d.chunk.css"
+  },
+  {
+    "revision": "87f8652a2bacd36221a3",
+    "url": "/quote/static/js/2.989eb93b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote/static/js/2.989eb93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e63daf1150b402132b",
+    "url": "/quote/static/js/main.f46f7f13.chunk.js"
+  },
+  {
+    "revision": "e951c9540d0e81762856",
+    "url": "/quote/static/js/runtime-main.e7068d47.js"
+  }
+]);
